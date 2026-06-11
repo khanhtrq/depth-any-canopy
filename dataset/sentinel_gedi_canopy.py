a200e53ed8fcb8d8f735e659b2ed852812425f26
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-06-11 16:25:52 +0700

    input transpose

--- dataset/sentinel_gedi_canopy.py
+++ dataset/sentinel_gedi_canopy.py
@@ -99,8 +99,8 @@ class GediSentinelDataset(Dataset):
         print(sentinel.shape, gedi.shape)
         print(sentinel.dtype, gedi.dtype)
 
-        print(sentinel.min(), sentinel.max())
-        print(gedi.nanmin(), gedi.nanmax())
+        print("Sentinel", sentinel.min(), sentinel.max())
+        print("GEDI", gedi.nanmin(), gedi.nanmax())
 
         sample = {
             "image": sentinel[1:4, :, :],  # using RGB bands only
