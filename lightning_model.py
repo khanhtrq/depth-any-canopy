3fa2d4ca424c54e81903c9c209607f81ff00129a
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-06-17 22:01:45 +0700

    prediction

--- lightning_model.py
+++ lightning_model.py
@@ -231,7 +231,7 @@ class DepthAnythingV2Module(LightningModule):
 
         valid_mask = ~torch.isnan(depth)
         num_valid = valid_mask.sum().item()
-        self.print(f"Valid pixels: {num_valid}")
+        # self.print(f"Valid pixels: {num_valid}")
 
         self.val_metric(pred[valid_mask], depth[valid_mask])
         metrics = self.val_metric.compute()
