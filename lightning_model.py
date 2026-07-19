da223d22cc119d516df0f9a43e1d972763820c54
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2026-07-19 15:30:23 +0700

    12 channel

--- lightning_model.py
+++ lightning_model.py
@@ -145,7 +145,8 @@ class DepthAnythingV2Module(LightningModule):
     def training_step(self, batch, batch_idx):
         img, depth = self._preprocess_batch(batch)
 
-        pred = self.model(img).predicted_depth
+        pred = self.model(img)
+        # .predicted_depth
         pred = resize(pred, depth.shape[-2:], interpolation="bilinear").clamp(0, 1)
 
         valid_mask = ~torch.isnan(depth)
